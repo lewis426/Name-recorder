dd7189a8ce945458c24b1158d6d8d843c9e2b34b
Lewis <lewis.easy@btinternet.com>
2018-05-08 10:38:57 +0100

    Initial Version

---
+++ program.py
@@ -0,0 +1,11 @@
+#Title of app
+print('----------------------------')
+print('        HELLO APP')
+print('----------------------------')
+print()
+
+#Taking user's input
+name = input('What is your name? ')
+
+#Processing out using input
+print('Your name is', name)
